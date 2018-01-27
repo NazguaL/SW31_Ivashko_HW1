f6a8d1f3a195f4c336c78ef371e58cc74ee811c5
NazguaL <nazgual@ukr.net>
2018-01-27 19:14:41 +0200

    4th

--- src/Triangle.java
+++ src/Triangle.java
@@ -2,10 +2,19 @@
  * Created by sergii.ivashko on 22.01.2018.
  */
 public class Triangle {
-
+    /**
+     *
+     * @param a
+     * @param b
+     * @param c
+     * @return
+     */
     public static boolean isTriangle(int a, int b, int c)
     {
-        if (a >= b + c||b >= a + c||c >= a + b) return false;
-        else return true;
+        if (a <= 0 || b <=0  || c <= 0) {return false;}
+        else if (a >= b + c || b >= a + c || c >= a + b) {return false;}
+
+            else return true;
+        }
     }
-}
+
